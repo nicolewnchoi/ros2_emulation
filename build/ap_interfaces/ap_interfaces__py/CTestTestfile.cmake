3c252df3c0be2495b43a1e8c350330653bba4726
ruotongg <ruotongg@umich.edu>
2022-06-15 19:01:39 -0400

    modify gitignore

--- build/ap_interfaces/ap_interfaces__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/18437/eecs281/ros2/build/ap_interfaces/ap_interfaces__py
-# Build directory: /mnt/c/Users/18437/eecs281/ros2/build/ap_interfaces/ap_interfaces__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/ap_interfaces/rosidl_generator_py/ap_interfaces/msg/__init__.py
+++
@@ -1,2 +0,0 @@
-from ap_interfaces.msg._pos import Pos  # noqa: F401
-from ap_interfaces.msg._score import Score  # noqa: F401
